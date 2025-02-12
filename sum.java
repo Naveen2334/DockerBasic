47a4d0fc76b8f3eb0165483fb984b2bff3c6e315
Naveen Kumar Barnwal <naveenkumar16613@gmail.com>
2025-02-12 14:36:16 +0530

    intial commit

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+
+    }
+}
