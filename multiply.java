ea772574c30753d0fdbe4b487a5e8d60518d7b9f
Naveen Kumar Barnwal <naveenkumar16613@gmail.com>
2025-02-12 18:37:08 +0530

    added new file

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        int a = 10;
+        int b = 20;
+        System.out.println(a*b);
+
+    }
+}
