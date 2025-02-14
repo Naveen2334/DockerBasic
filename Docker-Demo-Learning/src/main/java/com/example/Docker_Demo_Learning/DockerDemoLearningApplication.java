3ccbbf1125167fa729adc6a9fa786c5a051b7898
Naveen <naveenkumar16613@gmail.com>
2025-02-14 11:47:43 +0530

    first commit

---
+++ Docker-Demo-Learning/src/main/java/com/example/Docker_Demo_Learning/DockerDemoLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.Docker_Demo_Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoLearningApplication.class, args);
+	}
+
+}
